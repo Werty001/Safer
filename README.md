879759024faf45cc49091fba8f904aa053163e03
Agustin Ruggero <an_ruggero@hotmail.com>
2022-09-29 18:15:21 +0200

    Initial commit of Safer

---
+++ README.md
@@ -0,0 +1,18 @@
+# my_app
+
+A new Flutter project. This is the my first Flutter app. Is a test to learn new framewoks and how can i use and work with this.
+This project will be a Health, Safe and Enviroment Manager on mobile cross platform.
+It will be free and open.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:my_app/views/login-view.dart';
+import 'package:my_app/views/register_view.dart';
+
+import 'firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    title: 'Safer',
+    theme: ThemeData(
+      primarySwatch: Colors.orange,
+    ),
+    home: const HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                final user = FirebaseAuth.instance.currentUser;
+                if (user?.emailVerified ?? false) {
+                  print('You are a verified user');
+                } else {
+                  print('You need to verified your mail first');
+                }
+                return const Text('Done');
+              default:
+                return const Text('Loading...');
+            }
+          }),
+    );
+  }
+}
